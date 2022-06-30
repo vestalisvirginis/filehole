39d50f0e59d540dc31d04512b89bc37a1e58d981
Virginie Grosboillot <vestalisvirginis@gmail.com>
2022-06-30 15:29:20 +0200

    first test commit

---
+++ tests/units/test_filehole.py
@@ -0,0 +1,20 @@
+# Test class Globable
+
+# test _get_holidays
+
+# test filehole
+
+
+
+### Test functionality 
+
+# country
+# weekday
+# holidays/ no holidays
+# missing files
+# daily delivery
+# weekly delivery
+
+def test_pyhole(spark):
+    pass
+
